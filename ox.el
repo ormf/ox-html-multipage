c2430607a746b7bfd7807fd83537f20a511d4377
ormf <junk14@selma.hfmdk-frankfurt.de>
2024-07-22 20:13:22 +0200

    reverted org-export-transcode-headline.

--- ox.el
+++ ox.el
@@ -3099,30 +3099,34 @@ Return code as a string."
   "transcode the headline tree into a string according to the
 backend and return the string."
   (let* ((body (org-element-normalize-string
-		(or (org-export-data headline info)
-                    "")))
-	 (inner-template (cdr (assq 'inner-template)
-                              (plist-get info :translate-alist)))
+		(or (org-export-data headline info) "")))
+	 (inner-template (if (plist-get info :multipage)
+                             (alist-get 'multipage-inner-template
+                                        (plist-get info :translate-alist))
+                           (alist-get 'inner-template
+                                        (plist-get info :translate-alist))))
 	 (full-body (org-export-filter-apply-functions
 		     (plist-get info :filter-body)
 		     (if (not (functionp inner-template)) body
-                       (funcall inner-template body info))
+                         (funcall inner-template body info))
 		     info))
-	 (template (cdr (assq 'template
-                              (plist-get info :translate-alist))))
+	 (template (if (plist-get info :multipage)
+                       (cdr (assq 'multipage-template
+                                  (plist-get info :translate-alist)))
+                     (cdr (assq 'template
+                                  (plist-get info :translate-alist)))))
          (output
           (if (or (not (functionp template)) body-only) full-body
 	    (funcall template full-body info))))
     ;; Call citation export finalizer.
-    (when (plist-get info :with-cite-processors)
-      (setq output (org-cite-finalize-export output info))
-      ;; Remove all text properties since they cannot be
-      ;; retrieved from an external process.  Finally call
-      ;; final-output filter and return result.
-      (org-no-properties
-       (org-export-filter-apply-functions
-        (plist-get info :filter-final-output)
-        output info)))))
+    (setq output (org-cite-finalize-export output info))
+    ;; Remove all text properties since they cannot be
+    ;; retrieved from an external process.  Finally call
+    ;; final-output filter and return result.
+    (org-no-properties
+     (org-export-filter-apply-functions
+      (plist-get info :filter-final-output)
+      output info))))
 
 ;;;###autoload
 (defun org-export--annotate-info (backend info &optional subtreep visible-only ext-plist)
